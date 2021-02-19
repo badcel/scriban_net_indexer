ba89c4d43f27f73a05fa57a98fe7fcdd47ed5844
badcel <1218031+badcel@users.noreply.github.com>
2021-02-19 17:25:38 +0100

    Update

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Scriban;
+using Scriban.Runtime;
+
+namespace ConsoleApp1
+{
+    public class MyObjectHasIndexer 
+    {
+        public string this[string key]
+        {
+            get
+            {
+                if (key == "xxx")
+                    return "found";
+
+                return null;
+            }
+        }
+    }
+    
+    class Program
+    {
+        public static void TestIndexerOnNETwithIndex()
+        {
+            var myobject = new MyObjectHasIndexer();
+            var expected = myobject["xxx"];
+            var result = Template.Parse("{{obj['xxx']}}").Render(new ScriptObject() {{"obj", myobject}});
+        }
+        
+        static void Main(string[] args)
+        {
+            TestIndexerOnNETwithIndex();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # scriban_net_indexer
+
+Issue https://github.com/scriban/scriban/issues/159 indicates that .net indexer should work.
+
+I copied the sample and it looks like it is not working.
